24085802851fcc0f270ba916fcfdcad6e95cef60
viveklade <ladevivek33gmail.com>
2026-07-10 13:01:39 +0530

    fix the bug

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\View\View;
 use Throwable;
 
 class AttributeController extends Controller
